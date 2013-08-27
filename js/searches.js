7bc02d03c9981e39ab65318a3fd2d9030c638c32
jdungan <johnadungan@gmail.com>
2013-08-26 21:36:31 -0500

    new locations

--- js/searches.js
+++ js/searches.js
@@ -3,7 +3,7 @@ var searches={
     Team1:{
         lat :38.898662,
         lng :-77.039502,
-        name: 'OK TF#1 Team 1 in DC',
+        name: 'Penn and H in DC',
         user: {display_name:"OK TF#1 Team 1",
                 username:"OK TF#1 Team 1",
                 user_id:"61Jv-01",
@@ -12,7 +12,7 @@ var searches={
     Team2:{
         lat :38.900438,
         lng :-77.039574,
-        name:"OK TF#1 Team 2 in DC",
+        name:"17th and H in DC",
         user: {display_name:"OK TF#1 Team 2",
                 username:"OK TF#1 Team 2",
                 user_id:"61Jv-02",
@@ -25,9 +25,28 @@ var searches={
                 name:"91st & Memorial",
         user: {display_name:"91st & Memorial",
                 username:"91st & Memorial",
-                user_id:"61Jv-02",
+                user_id:"61Jv-03",
                 }
     },
+    POTUS:{
+        lat:38.898167,
+        lng:-77.036592,
+                name:"White House",
+        user: {display_name:"White House",
+                username:"White House",
+                user_id:"61Jv-04",
+                }
+    },
+    AIR:{
+        lat:38.887679,
+        lng:-77.020187,
+                name:"Air & Space",
+        user: {display_name:"Dream Big",
+                username:"Dream Big",
+                user_id:"61Jv-05",
+                }
+    },
+
 
 
 };
